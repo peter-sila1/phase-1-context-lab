156188fab3e729b409fda65620fc59839c22ce17
peter-sila1 <peter.sila@student.moringaschool.com>
2023-08-07 15:47:20 +0300

    context lab

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
  for you to use if you need it!
  */
 
+ 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,5 +20,51 @@ const allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+const createEmployeeRecord = ([firstName, familyName, title, payPerHour]) => {
+    const Obj = {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return Obj
 }
-
+const createEmployeeRecords = (arr) => { return arr.map(createEmployeeRecord) 
+}
+function createTimeInEvent(dateStamp) {
+    let Obj = {
+        type: 'TimeIn',
+        hour: parseInt(dateStamp.slice(-4), 10),
+        date: dateStamp.slice(0, 10)
+    }
+    this.timeInEvents.push(Obj)
+    return this
+}
+function createTimeOutEvent(dateStamp) {
+    let Obj = {
+        type: 'TimeOut',
+        hour: parseInt(dateStamp.slice(-4), 10),
+        date: dateStamp.slice(0, 10)
+    }
+    this.timeOutEvents.push(Obj)
+    return this
+}
+function hoursWorkedOnDate(dateStamp) {
+    let hourIn, hourOut
+    hourIn = this.timeInEvents.find((event) => event['date']===dateStamp)
+    hourOut = this.timeOutEvents.find((event) => event['date']===dateStamp)
+    return (hourOut.hour-hourIn.hour)/100
+}
+function allWagesFor(employeeObject) {
+    return employeeObject.timeInEvents.reduce((total, event) => {
+        return total + wagesEarnedOnDate(employeeObject, event.date)
+    }, 0)
+}
+function calculatePayroll(employeeRecord) {
+    return employeeRecord.reduce((total, employee) => {
+        return total + allWagesFor(employee)
+    }, 0)
+}
+}
